5d52e766cd2357cf42a2a9a261de38ad5b558125
hymntaha <tahauygun@gmail.com>
2020-10-02 11:19:48 -0700

    adding mongodb

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
+const mongodb = require('./mongodb/mongodb.connect');
+
 const todoRoutes = require('./routes/todo.routes');
 
+mongodb.connect();
 
+app.use(express.json());
 app.use('/todos', todoRoutes);
 
 app.get('/', (req, res)=>{
